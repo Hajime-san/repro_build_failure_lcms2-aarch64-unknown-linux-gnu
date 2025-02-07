130bd15960e2ec913696aab51aad989199aba294
Hajime-san <41257923+Hajime-san@users.noreply.github.com>
2025-02-07 22:11:19 +0900

    fix

--- build.rs
+++ build.rs
@@ -1,10 +1,10 @@
 use std::env;
-use std::process::Command;
 
 fn main() {
   let ci = env::var("CI");
+  #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
   if ci.is_ok() == true {
-    // #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
-    // println!("cargo:rustc-env=PKG_CONFIG_SYSROOT_DIR=/sysroot/usr/lib/aarch64-linux-gnu/pkgconfig");
+    println!("cargo:warning=this is a workaround for aarch64 linux");
+    println!("cargo:rustc-env=PKG_CONFIG_SYSROOT_DIR=/sysroot/usr/lib/aarch64-linux-gnu/pkgconfig")
   }
 }
