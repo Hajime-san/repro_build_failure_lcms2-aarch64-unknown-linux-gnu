2907adba1d89e1388a3e57ed5d4b9694774a1087
Hajime-san <41257923+Hajime-san@users.noreply.github.com>
2025-02-07 21:35:41 +0900

    test

--- build.rs
+++ build.rs
@@ -1,9 +1,10 @@
 use std::env;
+use std::process::Command;
 
 fn main() {
   let ci = env::var("CI");
   if ci.is_ok() == true {
-    #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
-    println!("cargo:warning=Hello, ARM64 Linux!");
+    // #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
+    // println!("cargo:rustc-env=PKG_CONFIG_SYSROOT_DIR=/sysroot/usr/lib/aarch64-linux-gnu/pkgconfig");
   }
 }
