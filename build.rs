d7ae5a30a5d52ef0faddc9143be3069e7f1a759b
Hajime-san <41257923+Hajime-san@users.noreply.github.com>
2025-02-07 21:25:50 +0900

    test

--- build.rs
+++ build.rs
@@ -1,14 +1,9 @@
 use std::env;
 
 fn main() {
-  #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
-  println!("Hello, ARM64 Linux!");
-
-  let target = env::var("CARGO_BUILD_TARGET");
-  match target {
-    Ok(target) => {
-      println!("build.target: {}", target);
-    }
-    Err(_) => {}
+  let ci = env::var("CI");
+  if ci.is_ok() == true {
+    #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
+    println!("cargo:warning=Hello, ARM64 Linux!");
   }
 }
