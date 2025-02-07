7571905b4804e447421facb5b2c1c1a065f023fd
Hajime-san <41257923+Hajime-san@users.noreply.github.com>
2025-02-07 22:17:29 +0900

    fix

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() {
   #[cfg(all(target_os = "linux", target_arch = "aarch64"))]
   if ci.is_ok() == true {
     println!("cargo:warning=this is a workaround for aarch64 linux");
-    println!("cargo:rustc-env=PKG_CONFIG_SYSROOT_DIR=/sysroot/usr/lib/aarch64-linux-gnu/pkgconfig");
-    println!("cargo:rustc-env=LCMS2_INCLUDE_DIR=/usr/lib/aarch64-linux-gnu");
+    // println!("cargo:rustc-env=PKG_CONFIG_SYSROOT_DIR=/sysroot/usr/lib/aarch64-linux-gnu/pkgconfig");
+    // println!("cargo:rustc-env=LCMS2_INCLUDE_DIR=/usr/lib/aarch64-linux-gnu");
   }
 }
