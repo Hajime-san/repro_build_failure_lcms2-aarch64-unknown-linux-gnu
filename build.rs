fc7eecea9a396f1c978a9ee50c8da6b3af01b654
Hajime-san <41257923+Hajime-san@users.noreply.github.com>
2025-02-10 12:39:18 +0900

    fix

--- build.rs
+++ build.rs
@@ -1,16 +1,10 @@
-use std::{env, process::Command};
+use std::env;
 
 fn main() {
   if let Ok(ci) = env::var("CI") {
     if ci == "true" {
-      let machine_path = Command::new("clang")
-        .arg("-dumpmachine")
-        .output()
-        .expect("failed to execute process");
-      let machine_path = std::str::from_utf8(&machine_path.stdout).unwrap();
       println!(
-        "cargo::rustc-env=PKG_CONFIG_LIBDIR=/sysroot/usr/lib/{}",
-        machine_path
+        "cargo::rustc-env=PKG_CONFIG_LIBDIR=/sysroot/usr/lib/aarch64-linux-gnu"
       );
     }
   }
